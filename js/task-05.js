2d9fba2e275ef24847fd67aa1ac2247434ef6032
Olena Sydorova <olena.sydorova@gmail.com>
2023-05-05 22:40:47 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,2 +1,7 @@
 const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
+inputEl.addEventListener("input", () => {
+  return inputEl.value
+    ? (outputEl.textContent = inputEl.value)
+    : (outputEl.textContent = "Anonymous");
+});
