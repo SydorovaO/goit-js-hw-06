df25b4e6bbd942975b3beef8fbe764ae66b14474
Olena Sydorova <olena.sydorova@gmail.com>
2023-05-05 19:30:57 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+
+const createIngredientsItemsEl = (ingredients) => {
+  return ingredients.map((ingredient) => {
+    const ingredientsItemEl = document.createElement("li");
+    ingredientsItemEl.textContent = ingredient;
+    ingredientsItemEl.classList.add("item");
+
+    return ingredientsItemEl;
+  });
+};
+
+const elements = createIngredientsItemsEl(ingredients);
+
+ingredientsListEl.append(...elements);
