78a01b7558fdf131bea6d0e53bb3eb5e389ce41a
Olena Sydorova <olena.sydorova@gmail.com>
2023-05-06 22:13:55 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,9 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const{elements:{email,password}} = event.curentTarget;
+
+//   if(email.value)
+// }
