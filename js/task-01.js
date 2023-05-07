b53b761b890f688c7662e3c76480abe34430d513
Olena Sydorova <olena.sydorova@gmail.com>
2023-05-07 17:17:38 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -4,8 +4,9 @@ console.log(`Number of categories: ${listEl.children.length}`);
 const listItemsEl = listEl.querySelectorAll(".item");
 
 listItemsEl.forEach((element) => {
-  const itemHeadingEl = element.querySelector("h2");
-  const itemListEl = element.querySelector("ul");
+  const itemHeadingEl = element.firstElementChild;
+  const itemListEl = element.lastElementChild;
+
   console.log(`Category: ${itemHeadingEl.textContent}`);
   console.log(`Elements: ${itemListEl.children.length}`);
 });
